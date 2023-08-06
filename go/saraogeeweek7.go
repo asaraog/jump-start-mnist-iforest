49bb97dc31b3cdbfb9588f75d7377ee6f92dcd5f
Asaraog <asaraog@Asaraog-ka-MacBook-Pro.local>
2023-08-05 20:37:21 -0700

    units for Go code

--- go/saraogeeweek7.go
+++ go/saraogeeweek7.go
@@ -14,45 +14,54 @@ import (
 
 func main() {
 
+	//Get Training images
+	images, _ := GetMNIST()
+
+	//Training of forest
+	f := iforest.NewForest(1000, 256, 0.0001) //initializes forest with 1000 trees, 256 samples, dummy threshold value (not used but maybe classifying)
+	f.Train(images)                           //uses BSTs to do partitioning
+	f.Test(images)                            // calculates anomaly scores for each sample
+
+	//Export
+	scores := ExtractScores(f) //extracts scores into a [][]string for export
+	file, _ := os.Create("../results/go_data.csv")
+	w := csv.NewWriter(file)
+	w.WriteAll(scores)
+}
+
+func GetMNIST() (images [][]float64, labels []int) {
+
 	//Loads data into [][]float64
 	train, _, _ := GoMNIST.Load("../data")
-	images := make([][]float64, len(train.Images))
-	labels := make([]int, len(train.Images))
+	images = make([][]float64, len(train.Images))
+	labels = make([]int, len(train.Images))
 
 	for i := 0; i < len(train.Images); i++ {
 		images[i] = make([]float64, len(train.Images[0]))
 		for p := range train.Images[0] {
-			images[i][p] = float64(train.Images[i][p])
+			images[i][p] = float64(train.Images[i][p]) //converts integer pixel values to float64 for input to iforest
 			labels[i] = int(train.Labels[i])
 
 		}
 	}
+	return images, labels
 
-	//Training of forest
-	TREES := 1000
-	samples := 256
-	threshold := 0.0001
-	f := iforest.NewForest(TREES, samples, threshold)
+}
 
-	f.Train(images)
-	f.Test(images)
-
-	//Export
-	file, _ := os.Create("../results/go_data.csv")
-	w := csv.NewWriter(file)
-	scores := make([][]string, len(f.AnomalyScores)+1)
+func ExtractScores(f *iforest.Forest) (scores [][]string) {
+	scores = make([][]string, len(f.AnomalyScores)+1)
 	for i := 0; i < len(scores); i++ {
 		scores[i] = make([]string, 2)
-		if i == 0 {
+		if i == 0 { //adds header in csv for easy import into R dataframes
 			scores[0][0] = "id"
 			scores[0][1] = "scores"
 		}
 		if i != 0 {
-			score := 0.5 - f.AnomalyScores[i-1]
+			score := 0.5 - f.AnomalyScores[i-1] //changes to conform with anomaly scores as defined by Liu 2008
 			scores[i][0] = fmt.Sprintf("%d", i-1)
 			scores[i][1] = fmt.Sprintf("%f", score)
 		}
 
 	}
-	w.WriteAll(scores)
+	return scores
 }
--- go/saraogeeweek7digit.go
+++ go/saraogeeweek7digit.go
@@ -15,61 +15,26 @@ import (
 func main() {
 
 	//Loads data into [][]float64
-	train, _, _ := GoMNIST.Load("../data")
-	images := make([][]float64, len(train.Images))
-	labels := make([]int, len(train.Images))
+	images, labels := GetMNIST()
 
-	for i := 0; i < len(train.Images); i++ {
-		images[i] = make([]float64, len(train.Images[0]))
-		for p := range train.Images[0] {
-			images[i][p] = float64(train.Images[i][p])
-			labels[i] = int(train.Labels[i])
-
-		}
-	}
-
-	samples := 256
-	threshold := 0.0001
 	//For filtering out a digit
 	for digit := 0; digit < 10; digit++ {
-		//Filters out digit into x
-		f := iforest.NewForest(100, samples, threshold)
-		x := make([][]float64, 0)
-		// intx := make([][]uint8, 0)
-		for i := range images {
-			if labels[i] == digit {
-				xx := make([]float64, len(images[0]))
-				// intxx := make([]uint8, len(images[0]))
-				for j := 0; j < len(images[0]); j++ {
-					xx[j] = float64(images[i][j])
-					// intxx[j] = uint8(images[i][j])
-				}
-				x = append(x, xx)
-				// intx = append(intx, intxx)
-			}
-		}
-		f.Train(x)
-		f.Test(x)
+		x, _ := DigitFilter(images, labels, digit) //creates new dataframe x for subset of training data for digit
+
+		//Training of forest
+		f := iforest.NewForest(100, 256, 0.0001) //reduced number of trees to 100 as fewer parameters than whole dataset
+		f.Train(x)                               //uses BSTs to do partitioning
+		f.Test(x)                                // calculates anomaly scores for each sample
+
+		//Export
+		scores := ExtractScores(f) //extracts scores into a [][]string for export
 		file, _ := os.Create(fmt.Sprintf("../results/digitanalysis/go_data_%d.csv", digit))
 		z := csv.NewWriter(file)
-		scores := make([][]string, len(f.AnomalyScores)+1)
-		for i := 0; i < len(scores); i++ {
-			scores[i] = make([]string, 2)
-			if i == 0 {
-				scores[0][0] = "id"
-				scores[0][1] = "scores"
-			}
-			if i != 0 {
-				score := 0.5 - f.AnomalyScores[i-1]
-				scores[i][0] = fmt.Sprintf("%d", i-1)
-				scores[i][1] = fmt.Sprintf("%f", score)
-			}
-		}
 		z.WriteAll(scores)
 
 		//Printing images with image package
 		// for i := 0; i < len(intx); i++ {
-		// 	if forest.Labels[i] == 1 {
+		// 	if forest.Labels[i] == 1 { //The Labels category depends on the contamination ratio used.
 		// 		pic := image.NewGray(image.Rect(0, 0, 28, 28))
 		// 		pic.Pix = intx[i]
 		// 		out, _ := os.Create("./" + fmt.Sprintf("%d_%d", digit, i) + ".png")
@@ -78,4 +43,60 @@ func main() {
 		// }
 
 	}
+}
+
+func GetMNIST() (images [][]float64, labels []int) {
+
+	//Loads data into [][]float64
+	train, _, _ := GoMNIST.Load("../data")
+	images = make([][]float64, len(train.Images))
+	labels = make([]int, len(train.Images))
+
+	for i := 0; i < len(train.Images); i++ {
+		images[i] = make([]float64, len(train.Images[0]))
+		for p := range train.Images[0] {
+			images[i][p] = float64(train.Images[i][p]) //converts integer pixel values to float64 for input to iforest
+			labels[i] = int(train.Labels[i])
+
+		}
+	}
+	return images, labels
+
+}
+
+func ExtractScores(f *iforest.Forest) (scores [][]string) {
+	scores = make([][]string, len(f.AnomalyScores)+1)
+	for i := 0; i < len(scores); i++ {
+		scores[i] = make([]string, 2)
+		if i == 0 { //adds header in csv for easy import into R dataframes
+			scores[0][0] = "id"
+			scores[0][1] = "scores"
+		}
+		if i != 0 {
+			score := 0.5 - f.AnomalyScores[i-1] //changes to conform with anomaly scores as defined by Liu 2008
+			scores[i][0] = fmt.Sprintf("%d", i-1)
+			scores[i][1] = fmt.Sprintf("%f", score)
+		}
+
+	}
+	return scores
+}
+
+func DigitFilter(images [][]float64, labels []int, digit int) (x [][]float64, intx [][]uint8) {
+	//For filtering out a dataframe of MNIST images by digit labels
+	x = make([][]float64, 0)  //initialize output array
+	intx = make([][]uint8, 0) //'intx' is the uint8 version for printing using image package
+	for i := range images {
+		if labels[i] == digit { //Calls out whether label at image index is equal to the input digit
+			xx := make([]float64, len(images[0])) //temporary variable
+			intxx := make([]uint8, len(images[0]))
+			for j := 0; j < len(images[0]); j++ {
+				xx[j] = float64(images[i][j])
+				intxx[j] = uint8(images[i][j])
+			}
+			x = append(x, xx)
+			intx = append(intx, intxx)
+		}
+	}
+	return x, intx
 }
